69b80a88550fd21d07d3d94c0260bf3ec382b6af
petrovengineer <37839368+petrovengineer@users.noreply.github.com>
2021-08-10 16:23:24 +0300

    make passes

--- server/report.js
+++ server/report.js
@@ -3,21 +3,25 @@ const fs = require('fs')
 const fetch = require('node-fetch')
 const {formatTime} = require('./time')
 
-const observations = [
-    {_id:'1', text:'Баланс счета сотовой связи: 123.00р.', photos:['https://claimtracker.s3.us-east-2.amazonaws.com/2021_7_11/17_42_e9d99d86-c6a6-4fd9-babe-a9ca51f2930c.jpg', 'https://claimtracker.s3.us-east-2.amazonaws.com/2021_7_11/17_42_e9d99d86-c6a6-4fd9-babe-a9ca51f2930c.jpg'], time: (new Date()).toISOString()},
-    {_id:'2', text:'QWQWEQWEQWEWE.', photos:[], time: (new Date()).toISOString()},
-]
+const JsBarcode = require('jsbarcode');
+const { Canvas } = require("canvas");
 
-const data = {
-    name: 'Скворцов Игорь',
-    observations,
-}
+// const observations = [
+//     {_id:'1', text:'Баланс счета сотовой связи: 123.00р.', photos:['https://claimtracker.s3.us-east-2.amazonaws.com/2021_7_11/17_42_e9d99d86-c6a6-4fd9-babe-a9ca51f2930c.jpg', 'https://claimtracker.s3.us-east-2.amazonaws.com/2021_7_11/17_42_e9d99d86-c6a6-4fd9-babe-a9ca51f2930c.jpg'], time: (new Date()).toISOString()},
+//     {_id:'2', text:'QWQWEQWEQWEWE.', photos:[], time: (new Date()).toISOString()},
+// ]
 
-async function createDoc(){
-    const template = fs.readFileSync('./templates/report.docx');
+// const data = {
+//     name: 'Скворцов Игорь',
+//     observations,
+// }
 
+// const template = './templates/report.docx';
+// const output = './docs/report.docx';
+
+async function createDoc(template, output, data){
     const buffer = await createReport({
-      template,
+      template: fs.readFileSync(template),
       cmdDelimiter: ['{', '}'],
       additionalJsContext:{
         getImage: async (url)=>{
@@ -25,14 +29,47 @@ async function createDoc(){
             const buffer = await response.buffer();
             return { width: 16, height: 12, data: buffer, extension: '.jpg' }; 
         },
+        getBarcode: (num)=>{
+          const canvas = new Canvas();
+          JsBarcode(canvas, JSON.stringify({renter:data.renter, num}),
+          {displayValue: false}
+          );
+          const buf = canvas.toBuffer();
+          return {width: 16, height: 3, data: buf, extension:'.png'}
+        },
         formatTime
       },
       data
     });
     
-    fs.writeFileSync('./docs/report.docx', buffer)
+    fs.writeFileSync(output, buffer)
 }
 
-createDoc()
+
+
+function createPasses(data, count){
+  const template = './templates/pass.docx';
+  for(let i=1; i<=count; i++){
+    const output = `./docs/pass-${data.renter}-${i}.docx`;
+    const newData = Object.assign({num:i}, data)
+    createDoc(template, output, newData)
+  }
+}
+
+function createDublePasses(data, count){
+  const template = './templates/pass-duble.docx';
+  for(let i=1; i<=count; i=i+2){
+    const output = `./docs/pass-${data.renter}-${i},${i+1}.docx`;
+    const newData = Object.assign({num:i, first:i, second:i+1}, data)
+    createDoc(template, output, newData)
+  }
+}
+
+const data = {
+  renter: 'oodji',
+  renterRu: 'Оджи'
+}
+
+createDublePasses(data, 6)
 
 module.exports = {createDoc}
